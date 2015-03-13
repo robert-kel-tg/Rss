7805d23bc77875ad3a2c78013cdd973d3af640f8
Robert <kel.robert@gmail.com>
2015-03-13 09:38:43 +0000

    Mod for feeds file

--- feeds.php
+++ feeds.php
@@ -4,20 +4,30 @@
 
 use Rss\Src\RssEntityManager;
 use Rss\Src\Config;
+use Rss\Src\Twig;
+use Rss\Src\DoctrineRssRepository;
+use Rss\Entity\Feed;
 
-$rssEntityManager = new RssEntityManager(new Config());
-$feedRepository = $rssEntityManager->getRepository('Rss\Entity\Feed');
-$dbfeed = $feedRepository->findOneBy(array('category'=>$category));
+$twig = new Twig(
+    new Twig_Loader_Filesystem('Templates'),
+    new Twig_Environment()
+);
 
-$loader = new Twig_Loader_Filesystem('Templates');
-$twig = new Twig_Environment($loader);
+$doctrineRssRepository = new DoctrineRssRepository(new RssEntityManager(new Config()));
+$dbFeed = $doctrineRssRepository->findFeedByName($category);
 
-if($dbfeed) {
-    $itemRepository = $rssEntityManager->getRepository('Rss\Entity\Item');
-    $itemscount = $itemRepository->findBy(array('feed_id' => $dbfeed->getId()));
-    $recentitem = $itemRepository->findOneBy(array(), array('id' => 'ASC'), 1, 0);
+if ($dbFeed instanceof Feed) {
+    $items = $doctrineRssRepository->getItemsByFeedId($dbFeed->getId());
 
-    echo $twig->render('app.html', array('feed' => $dbfeed, 'feeds_count' => count($itemscount), 'recent_item' => $recentitem));
+    $iterator = new ArrayIterator($items);
+    $countAll = $iterator->count();
+    $iterator = new LimitIterator($iterator, 0, 1);
+
+    $iterator->rewind();
+
+    echo $twig->render('app.html',
+        array('feed' => $dbFeed, 'feeds_count' => $countAll, 'recent_item' => $iterator->current()));
 } else {
-    echo $twig->render('empty.html', array('empty_text' => 'In order to see something, import rss (f.g http://www.nfq.lt/rss) to DB via: php console.php grab:rss.'));
+    echo $twig->render('empty.html',
+        array('empty_text' => 'In order to see something, import rss (f.g http://www.nfq.lt/rss) to DB via: php console.php grab:rss.'));
 }
